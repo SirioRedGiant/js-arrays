47de3b626e8dc67dfce5d50b73d994ab63e994d3
SirioRedGiant <Oiris1992@gmail.com>
2025-12-18 17:20:45 +0100

    Creando spaziatura tra gli esercizi

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -1,13 +1,21 @@
 const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; // NON MODIFICARE QUESTA VARIABILE
 
+//! Colori per formattazione risultati
+
+//todo -> Colore Arancio per le stampe di array
+//  Violetto per i booleani
+
+
+console.log("Soluzione Esercizi: \n \n ")
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
 const reversedTeachers = [];
 for (let i = teachers.length - 1; i >= 0; i--) {
   reversedTeachers.push(teachers[i]);
 }
+console.log("Esercizio 1")
 console.log(reversedTeachers);
-console.log("%c" + reversedTeachers, "background-color: orange; color: black; padding: 2px;");
+console.log("%c" + reversedTeachers.join(", "), "background-color: orange; color: black; padding: 2px;");
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
@@ -31,11 +39,14 @@ for (let i = 0; i < teachers.length; i++) {
     longNames.push(teachers[i]);
   }
 }
+console.log("Esercizio 2 \n ")
 console.log(longNames);
+console.log("%c" + longNames.join(", "), "background-color: orange; color: black; padding: 2px;");
 
 // 3. Rimuovi 'Ed' dall'array teachers
 teachers.splice(1, 1);
 console.log(teachers);
+console.log("%c" + teachers.join(", "), "background-color: orange; color: black; padding: 2px;");
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 let isFabioPresent = false;
@@ -45,6 +56,7 @@ for (let i = 0; i < teachers.length; i++) {
   }
 }
 console.log(isFabioPresent);
+console.log("%c" + isFabioPresent, "background-color: rgba(218, 34, 238, 1) ; color: black; padding: 2px;");
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 let teachersString = "";
@@ -52,3 +64,4 @@ for (let i = 0; i < teachers.length; i++) {
   teachersString += teachers[i] + ", ";
 }
 console.log(teachersString);
+console.log("%c" + teachersString.trim().slice(0, -1), "background-color: orange; color: black; padding: 2px;");
