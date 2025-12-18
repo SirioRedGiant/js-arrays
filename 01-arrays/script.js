4019a965cc9e41ad524fae6d4082e0c8a8bcfff0
SirioRedGiant <Oiris1992@gmail.com>
2025-12-18 14:12:17 +0100

    Applied prettier

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -6,25 +6,25 @@ const fourthTeacher = teachers[3];
 console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-const fifthTeacherSwap = teachers[4] = "Carlo"
-console.log(fifthTeacherSwap)
+const fifthTeacherSwap = (teachers[4] = "Carlo");
+console.log(fifthTeacherSwap);
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
-console.log(lastTeacher)
+console.log(lastTeacher);
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers.shift();
-console.log(firstTeacher)
+console.log(firstTeacher);
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-teachers.push("Vanessa")
-console.log(teachers)
+teachers.push("Vanessa");
+console.log(teachers);
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
-teachers.unshift("Sarah")
-console.log(teachers)
+teachers.unshift("Sarah");
+console.log(teachers);
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
 const lewisIndex = teachers[5];
-console.log(lewisIndex)
+console.log(lewisIndex);
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
 const isTeachersEmpty = teachers.length === 0;
-console.log(isTeachersEmpty)
+console.log(isTeachersEmpty);
--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -6,7 +6,7 @@ const reversedTeachers = [];
 for (let i = teachers.length - 1; i >= 0; i--) {
   reversedTeachers.push(teachers[i]);
 }
-console.log(reversedTeachers)
+console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
@@ -18,27 +18,27 @@ for teacher in teachers:
  */
 for (let teacher of teachers) {
   if (teacher.length >= 5) {
-    longNames.push(teacher)
+    longNames.push(teacher);
   }
 }
-console.log(longNames)
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
-teachers.splice(1, 1)
-console.log(teachers)
+teachers.splice(1, 1);
+console.log(teachers);
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 let isFabioPresent = false;
 for (let i = 0; i < teachers.length; i++) {
   if (teachers[i] === "Fabio") {
-    isFabioPresent = true
+    isFabioPresent = true;
   }
 }
-console.log(isFabioPresent)
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 let teachersString = "";
 for (let i = 0; i < teachers.length; i++) {
-  teachersString += teachers[i] + ", "
+  teachersString += teachers[i] + ", ";
 }
-console.log(teachersString)
+console.log(teachersString);
