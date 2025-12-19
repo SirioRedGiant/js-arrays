1dc760fe78ae3b2c650d20b64f14194d5bec1096
SirioRedGiant <Oiris1992@gmail.com>
2025-12-19 09:38:03 +0100

    fix

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -7,7 +7,7 @@ console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 teachers[4] = "Patrick"
-console.log(fifthTeacherSwap);
+console.log(teachers);
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
 console.log(lastTeacher);
